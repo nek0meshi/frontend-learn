e744baa2c97c1081a78ca9fcf6dd05f91077e847
nek0meshi <nekmeshi@gmail.com>
2021-12-25 13:27:30 +0900

    - gulp-uglifyを追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 exports.default = () => {
   return gulp.src('src/**/*.js')
     .pipe(babel({
       presets: ['@babel/preset-env']
     }))
+    .pipe(uglify())
     .pipe(gulp.dest('dist'))
 }
