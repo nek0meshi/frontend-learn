f339231ef014d2947c74fa817c9416011ba1dc14
nek0meshi <nekmeshi@gmail.com>
2021-12-25 15:25:12 +0900

    - sassをトランスパイルできるようにする

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-const gulp = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const sass = require('gulp-sass')(require('sass'))
+const uglify = require('gulp-uglify')
 
 const scripts = () => {
   return gulp.src('src/**/*.js')
@@ -11,6 +12,17 @@ const scripts = () => {
     .pipe(gulp.dest('dist'))
 }
 
+const styles = () => {
+  return gulp.src('src/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist'))
+}
+
 exports.scripts = scripts
 
-exports.default = gulp.parallel(exports.scripts)
+exports.styles = styles
+
+exports.default = gulp.parallel(
+  scripts,
+  styles
+)
